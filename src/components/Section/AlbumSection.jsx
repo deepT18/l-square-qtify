3a3045c6229945c72a362b4a9b0aacbc3ca9ac33
DeeptenduKuri <deeptendukuri@gmail.com>
2023-12-28 23:38:26 +0530

    grid commit4

--- src/components/Section/AlbumSection.jsx
+++ src/components/Section/AlbumSection.jsx
@@ -1,31 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Card from "../Card/Card.jsx";
 import Grid from "@mui/material/Grid";
 import styles from "./Section.module.css";
+import { CircularProgress } from "@mui/material";
 
-
-
-const AlbumSection = ({albumDatas, title}) => {
-
-  const [topAlbums, setTopAlbums] = useState([]);
+const AlbumSection = ({ data, title }) => {
   const [toggle, setToggle] = useState(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://qtify-backend-labs.crio.do/albums/top"
-        );
-        const data = response.data;
-        setTopAlbums(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
 
   const handleToggle = () => {
     setToggle(!toggle);
@@ -36,22 +17,26 @@ const AlbumSection = ({albumDatas, title}) => {
       <div className={styles.headingShow}>
         <h3>{title}</h3>
         <h4 onClick={handleToggle} className={styles.toggleText}>
-          {toggle ? "Collapse":"Show All"}
+          {toggle ? "Collapse" : "Show All"}
         </h4>
-        </div>
-      {toggle ? (
-        <div className={styles.wrapper}>
-          <Grid container spacing={2}>
-            {topAlbums.map((album) => (
-              <Grid item key={album.id} xs={12} sm={6} md={6} lg={2}>
-                <Card album={album} key={album.id} />
-              </Grid>
-            ))}
-          </Grid>
-        </div>
-      ) : null}
+      </div>
+  
+      {data && data.length === 0 ? (
+        <CircularProgress />
+      ) : (
+        toggle ? (
+          <div className={styles.wrapper}>
+            <Grid container spacing={2}>
+              {data.map((album) => (
+                <Grid item key={album.id} xs={12} sm={6} md={6} lg={2}>
+                  <Card album={album} key={album.id} />
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+        ) : null
+      )}
     </div>
   );
-};
-
+              }; 
 export default AlbumSection;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import AlbumSection from "./AlbumSection"
-import {fetchNewAlbums,fetchSongs,fetchTopAlbums} from "../../apis/api"
+import React,{useEffect,useState} from 'react'
+import AlbumSection from "./AlbumSection.jsx"
+import {fetchNewAlbums,fetchSongs,fetchTopAlbums} from "../../apis/api.js"
 
 
 const Section = () => {
+    const [topAlbumSongs, setTopAlbumSongs] = useState([])
+    const [newAlbumSongs, setNewAlbumSongs] = useState([])
+
+    const generateTopAlbumSongs = async () => {
+        try {
+          const topAlbumSongs = await fetchTopAlbums()
+          setTopAlbumSongs(topAlbumSongs)
+        }
+        catch (error) {
+          console.log(error)
+          return null
+        }
+    
+      }
+      const generateNewAlbumSongs = async () => {
+        try {
+          const newAlbumSongs = await fetchNewAlbums()
+          console.log(newAlbumSongs);
+
+          setNewAlbumSongs(newAlbumSongs);
+          // setFilteredDataValues(newAlbumSongs);
+        }
+        catch (error) {
+          console.log(error)
+          return null
+        }
+      }
+    
+
+    useEffect(() => {
+    
+        generateTopAlbumSongs();
+        generateNewAlbumSongs();
+        // setFilteredDataValues(newAlbumSongs);
+    
+      }, [])
+
   return (
     <div>
-        <AlbumSection fetchTopAlbums title="Top Albums" />
-        <AlbumSection fetchNewAlbums title="New Albums" />
+        <AlbumSection data={topAlbumSongs} title="Top Albums" />
+        <AlbumSection data={newAlbumSongs} title="New Albums" />
         
         
     </div>
